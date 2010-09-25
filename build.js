d6082ad6914777db2dc905b60593078af7a67f25
Bastian Müller <bastian@turbolent.com>
2010-09-25 23:42:36 +0200

    improved build script

--- build.js
+++ build.js
@@ -1,19 +1,52 @@
 var system = require('system');
 var fs = require('fs');
 
-// boostrap compiler
-
 var compiler = require('./bootstrap/compiler');
 
-/*
-compiler.compile
-
-var data = fs.read(filename);
-
-
-    fs.write(fs.base(filename, '.ralph') + '.js',
-	      + '\n');
+function compileFile (path) {
+    fs.write((fs.directory(path) + '/' + 
+	      fs.base(path, '.ralph') + '.js'),
+	     compiler.compile(fs.read(path)) + '\n');
 }
 
-main();
-*/
+var commands = {
+    clean: function () {
+	// clean runtime
+	system.print('Cleaning runtime ...');
+	fs.listTree('./runtime/').forEach(function (path) {
+	    var path = './runtime/' + path;
+	    if (fs.extension(path) == '.js') {
+		system.print(' - ' + path);
+		fs.removeTree(path);
+	    }
+	});
+	// clean tests 
+	system.print('Cleaning tests ...');
+	fs.removeTree('./tests.js');
+    },
+    bootstrapRuntime: function () {
+	system.print('Bootstraping runtime ...');
+	fs.listTree('./runtime/').forEach(function (path) {
+	    var path = './runtime/' + path;
+	    if (fs.extension(path) == '.ralph' &&
+		fs.base(path, '.ralph')[0] != '.')
+	    {
+		system.print(' - ' + path);
+		compileFile(path);
+	    }
+	});
+    },
+    compileTestLibrary: function () {
+	system.print('Compiling test library ...');
+	compileFile(path);
+    },
+    compileTests: function () {
+	system.print('Compiling tests ...');
+	compileFile('./tests.ralph');
+    }
+}
+
+system.args.slice(1).forEach(function (command) {
+    if (commands.hasOwnProperty(command))
+	commands[command]();
+});
