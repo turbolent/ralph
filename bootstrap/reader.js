cc13e1add0207066fb2bd43341f6e68527cfec94
Bastian Müller <bastian@turbolent.com>
2010-10-22 21:36:14 +0200

    fix: parse optional sign in number expressions

--- bootstrap/reader.js
+++ bootstrap/reader.js
@@ -26,9 +26,9 @@ Reader.prototype.read = function () {
     }
 }
 
-var hexNumberExpression = /^0x[0-9a-f]+/i;
-var octalNumberExpression = /^0[0-7]+/;
-var decimalNumberExpression = /^\d+\.?\d*(?:e-?\d*(?:\d\.?|\.?\d)\d*)?/i;
+var hexNumberExpression = /^[+-]?0x[0-9a-f]+/i;
+var octalNumberExpression = /^[+-]?0[0-7]+/;
+var decimalNumberExpression = /^[+-]?\d+\.?\d*(?:e-?\d*(?:\d\.?|\.?\d)\d*)?/i;
 
 Reader.prototype.readAtom = function () {
     var c = this.stream.peekChar();
