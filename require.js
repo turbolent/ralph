219b85689cc773b6e0780c58722ee032d85eb88c
Bastian Müller <bastian@turbolent.com>
2011-06-02 13:49:30 +0200

    support for local files

--- require.js
+++ require.js
@@ -9,12 +9,19 @@ function _e (exports, _code) {
      var requests = 0;
      var deferredCallbacks = [];
 
+     function dirname (path) {
+         return path.substring(0, path.lastIndexOf('/') + 1);
+     }
+
      function request (name, callback) {
          var req = new XMLHttpRequest();
-         req.open("GET", document.location.href + name + ".js", true);
+         var path = name + ".js";
+         req.open("GET", dirname(document.location.href) + path, true);
          req.onreadystatechange = function () {
              if (req.readyState == 4) {
-                 if (req.status == 200) {
+                 if ((req.status == 200)
+                     || (req.status == 0))
+                 {
                      var currentRequests = requests;
                      var exports = {};
                      _e(exports, req.responseText);
