e7f4683eddf2893acca0a050e97ced846dc9ec5f
Bastian Mueller <bastian@turbolent.com>
2011-08-13 12:57:07 +0000

    server: optional optimization using google's closure compiler

--- compiler.js
+++ compiler.js
@@ -17,15 +17,39 @@ function handler (req, res) {
                    var sourceStream =
                        core.make(stream._CL_stringStream, core._k("string"),
                                  "(begin " + code + ")");
-				   sourceStream.line = 1;
+                   sourceStream.line = 1;
                    var form = reader.read(sourceStream);
                    var options = url.parse(req.url, true).query;
                    var compiled = compiler.compile(form,
                                                    core._k('statements?'), ((options && options.hasOwnProperty('statements'))
                                                                             ? !!options.statements : true),
                                                    core._k('asynchronous?'), options && !!options.asynchronous);
-                   res.writeHead(200, {'Content-Type': 'text/javascript'});
-                   res.end(compiled);
+                   if (options.optimize) {
+                       var req2 = http.request({host: 'closure-compiler.appspot.com',
+                                                path: '/compile',
+                                                method: 'POST',
+                                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                                               },
+                                               function (res2) {
+                                                   var optimized = '';
+
+                                                   res2.on('data', function (chunk) {
+                                                               optimized += chunk.toString();
+                                                           });
+
+                                                   res2.on('end', function () {
+                                                               res.writeHead(200, {'Content-Type': 'text/javascript'});
+                                                               res.end(optimized);
+                                                           });
+                                               });
+                       req2.write("compilation_level=ADVANCED_OPTIMIZATIONS&js_code="
+                                  + encodeURIComponent(compiled)
+                                  + "&output_format=text&output_info=compiled_code");
+                       req2.end();
+                   } else {
+                       res.writeHead(200, {'Content-Type': 'text/javascript'});
+                       res.end(compiled);
+                   }
                } catch (e) {
                    res.writeHead(500);
                    res.end(e.stack.toString());
